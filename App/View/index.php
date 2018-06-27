9e8eef998d8090d343955c1ce933d737f9aa2b59
yaoyao <32184094+yynan555@users.noreply.github.com>
2018-06-27 14:45:05 +0800

    调整左侧菜单宽度

--- App/View/index.php
+++ App/View/index.php
@@ -5,56 +5,64 @@
     <meta charset="utf-8">
     <meta name="renderer" content="webkit|ie-comp|ie-stand">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no" />
-    <meta http-equiv="Cache-Control" content="no-siteapp" />
-    <link rel="stylesheet" type="text/css" href="<?=STATIC_PATH?>/lib/h-ui/css/H-ui.min.css" />
-    <link rel="stylesheet" type="text/css" href="<?=STATIC_PATH?>/lib/h-ui.admin/css/H-ui.admin.css" />
-    <link rel="stylesheet" type="text/css" href="<?=STATIC_PATH?>/lib/Hui-iconfont/1.0.8/iconfont.css" />
-    <link rel="stylesheet" type="text/css" href="<?=STATIC_PATH?>/lib/h-ui.admin/skin/default/skin.css" id="skin" />
-    <link rel="stylesheet" type="text/css" href="<?=STATIC_PATH?>/lib/h-ui.admin/css/style.css" />
-    <link rel="stylesheet" type="text/css" href="<?=STATIC_PATH?>/lib/jstree/jstree.css" />
+    <meta name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"/>
+    <meta http-equiv="Cache-Control" content="no-siteapp"/>
+    <link rel="stylesheet" type="text/css" href="<?= STATIC_PATH ?>/lib/h-ui/css/H-ui.min.css"/>
+    <link rel="stylesheet" type="text/css" href="<?= STATIC_PATH ?>/lib/h-ui.admin/css/H-ui.admin.css"/>
+    <link rel="stylesheet" type="text/css" href="<?= STATIC_PATH ?>/lib/Hui-iconfont/1.0.8/iconfont.css"/>
+    <link rel="stylesheet" type="text/css" href="<?= STATIC_PATH ?>/lib/h-ui.admin/skin/default/skin.css" id="skin"/>
+    <link rel="stylesheet" type="text/css" href="<?= STATIC_PATH ?>/lib/h-ui.admin/css/style.css"/>
+    <link rel="stylesheet" type="text/css" href="<?= STATIC_PATH ?>/lib/jstree/jstree.css"/>
 
-    <link rel="stylesheet" type="text/css" href="<?=STATIC_PATH?>/css/index.css" />
+    <link rel="stylesheet" type="text/css" href="<?= STATIC_PATH ?>/css/index.css"/>
 </head>
 <body>
-<aside class="Hui-aside" style="top:0px; padding-top:0px">
-    <div style="width:190px">
-        当前用户:<?=\App\Core\UserAuth::session('username');?>
+<aside class="Hui-aside" id="left_area">
+    <div id="user_center" style="width:190px">
+        当前用户:<?= \App\Core\UserAuth::session('username'); ?>
         <hr/>
         <button class="btn btn-primary-outline radius size-MINI" onclick="reload_tree()">刷新目录</button>
-        <?php if(\App\Core\UserAuth::accessDatabaseAble()):?>
-        <a class="btn btn-primary-outline radius size-MINI" target="_blank" href='<?=BASE_URL?>/plugins/adminer' >数据库管理</a>
-        <br/>
-        <?php endif;?>
-        <a class="btn btn-primary-outline radius size-MINI" href='<?=BASE_URL?>/index.php?a=set_password' >修改密码</a>
-        <a class="btn btn-primary-outline radius size-MINI" href='<?=BASE_URL?>/index.php?a=logout' >退出登录</a>
+        <?php if (\App\Core\UserAuth::accessDatabaseAble()): ?>
+            <a class="btn btn-primary-outline radius size-MINI" target="_blank" href='<?= BASE_URL ?>/plugins/adminer'>数据库管理</a>
+            <br/>
+        <?php endif; ?>
+        <a class="btn btn-primary-outline radius size-MINI" href='<?= BASE_URL ?>/index.php?a=set_password'>修改密码</a>
+        <a class="btn btn-primary-outline radius size-MINI" href='<?= BASE_URL ?>/index.php?a=logout'>退出登录</a>
         <hr/>
     </div>
     <div class="" id="tree"></div>
 </aside>
-<div class="dislpayArrow hidden-xs"><a class="pngfix" href="javascript:void(0);" onClick="displaynavbar(this)"></a></div>
+<div class="dislpayArrow hidden-xs">
+    <a class="pngfix" href="javascript:void(0);" onClick="_displaynavbar(this)"></a>
+</div>
 <section class="Hui-article-box" style="top:0px">
     <div id="Hui-tabNav" class="Hui-tabNav hidden-xs">
         <div class="Hui-tabNav-wp">
-            <ul id="min_title_list" class="acrossTab cl">
-            </ul>
+            <ul id="min_title_list" class="acrossTab cl"></ul>
         </div>
-        <div class="Hui-tabNav-more btn-group"><a id="js-tabNav-prev" class="btn radius btn-default size-S" href="javascript:;"><i class="Hui-iconfont">&#xe6d4;</i></a><a id="js-tabNav-next" class="btn radius btn-default size-S" href="javascript:;"><i class="Hui-iconfont">&#xe6d7;</i></a></div>
+        <div class="Hui-tabNav-more btn-group">
+            <a id="js-tabNav-prev" class="btn radius btn-default size-S" href="javascript:;">
+                <i class="Hui-iconfont">&#xe6d4;</i>
+            </a>
+            <a id="js-tabNav-next" class="btn radius btn-default size-S" href="javascript:;">
+                <i class="Hui-iconfont">&#xe6d7;</i>
+            </a>
+        </div>
     </div>
-    <div id="iframe_box" class="Hui-article">
-    </div>
+    <div id="iframe_box" class="Hui-article"></div>
 </section>
 
-<script type="text/javascript" src="<?=STATIC_PATH?>/lib/jquery/1.9.1/jquery.min.js"></script>
-<script type="text/javascript" src="<?=STATIC_PATH?>/lib/h-ui/js/H-ui.min.js"></script>
-<script type="text/javascript" src="<?=STATIC_PATH?>/lib/h-ui.admin/js/H-ui.admin.js"></script>
-<script type="text/javascript" src="<?=STATIC_PATH?>/lib/jstree/jstree.js"></script>
-<script type="text/javascript" src="<?=STATIC_PATH?>/lib/layer/2.4/layer.js"></script>
+<script type="text/javascript" src="<?= STATIC_PATH ?>/lib/jquery/1.9.1/jquery.min.js"></script>
+<script type="text/javascript" src="<?= STATIC_PATH ?>/lib/h-ui/js/H-ui.min.js"></script>
+<script type="text/javascript" src="<?= STATIC_PATH ?>/lib/h-ui.admin/js/H-ui.admin.js"></script>
+<script type="text/javascript" src="<?= STATIC_PATH ?>/lib/jstree/jstree.js"></script>
+<script type="text/javascript" src="<?= STATIC_PATH ?>/lib/layer/2.4/layer.js"></script>
 
 <script type="text/javascript">
-var base_url = '<?=BASE_URL?>';
+    var base_url = '<?=BASE_URL?>';
 </script>
-<script type="text/javascript" src="<?=STATIC_PATH?>/js/index.js"></script>
-<script type="text/javascript" src="<?=STATIC_PATH?>/js/jstree.js"></script>
+<script type="text/javascript" src="<?= STATIC_PATH ?>/js/index.js"></script>
+<script type="text/javascript" src="<?= STATIC_PATH ?>/js/jstree.js"></script>
 </body>
 </html>
--- public/static/js/index.js
+++ public/static/js/index.js
@@ -99,3 +99,71 @@ function global_downloadByUrl(url) {
         console.log('download error',e)
     } 
 }
+
+// 左侧框拉动事件
+$(function(){
+    if(typeof(localStorage) != 'undefined'){
+        menu_width = localStorage.getItem('menu_width');
+        if(menu_width){
+            changeLeftSize(menu_width);
+        }
+    }
+
+    var is_on_border = false;
+    var allow_resize = false;
+    $('body').bind({
+        mousemove:function(e){
+            // 得到左侧边框位置
+            var la_width = $('#left_area').width();
+            var la_outerWidth = $('#left_area').outerWidth();
+            var x = e.clientX;
+            if(x>(la_width-3) && x<la_outerWidth){
+                $('#left_area').css("cursor",'e-resize');
+                is_on_border = true;
+            }else{
+                $('#left_area').css("cursor",'');
+                is_on_border = false;
+            }
+
+            // 若鼠标按下 就可以进行窗口大小改变
+            if(allow_resize){
+                changeLeftSize(x);
+                if(typeof(localStorage) != 'undefined'){
+                    localStorage.setItem('menu_width',x)
+                }
+            }
+        },
+        mousedown:function(e){
+            if(is_on_border){
+                allow_resize = true;
+            }
+        },
+        mouseup:function(){
+            allow_resize = false;
+        }
+    });
+});
+
+// 改变左侧窗口大小
+function changeLeftSize(width){
+    $('#left_area').css("width",width); //左侧宽度
+    $('.dislpayArrow').offset({"left":parseInt(width)+3}); // 左侧缩回按钮
+    $('.Hui-article-box').offset({"left":width}); // 右侧区域
+    $('#user_center').css("width",width); // 用户中心
+}
+/*左侧菜单-隐藏显示*/
+function _displaynavbar(obj){
+    $left_area = $('#left_area');
+    if($(obj).hasClass("open")){
+        $(obj).removeClass("open");
+        $left_area.css("left",0);
+        $('.dislpayArrow').css("left",$left_area.width()+3);
+        $('.Hui-article-box').css("left",$left_area.width());
+    }else{
+        $(obj).addClass("open");
+        var la_outerWidth = $left_area.outerWidth()+1;
+        $left_area.css("left",-la_outerWidth);
+        $('.dislpayArrow').css("left", 1);
+        $('.Hui-article-box').css("left",0);
+    }
+}
